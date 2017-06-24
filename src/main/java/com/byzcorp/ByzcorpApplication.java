fe5dabac9ad435f75761310ed899faa278811364
bayram.beyaz <bayram.beyaz@akgunyazilim.com.tr>
2017-06-24 03:12:06 +0300

    Deploying byzCorp web services to tomcat with war file

--- src/main/java/com/byzcorp/ByzcorpApplication.java
+++ src/main/java/com/byzcorp/ByzcorpApplication.java
@@ -1,13 +1,26 @@
 package com.byzcorp;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
-@SpringBootApplication
-public class ByzcorpApplication {
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+public class ByzcorpApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(ByzcorpApplication.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
+	private static Class<ByzcorpApplication> applicationClass = ByzcorpApplication.class;
 }
 
